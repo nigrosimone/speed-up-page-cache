d886bc45ad2056c4497e0e8876fd31f74575e9a1
nigrosimone <310077+nigrosimone@users.noreply.github.com>
2026-07-26 14:57:36 +0000

    Release 1.0.24

--- speed-up-page-cache.php
+++ speed-up-page-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: Speed Up - Page Cache
  * Plugin URI: http://wordpress.org/plugins/speed-up-page-cache/
  * Description: A simple page caching plugin.
- * Version: 1.0.23
+ * Version: 1.0.24
  * Author: Simone Nigro
  * Author URI: https://profiles.wordpress.org/nigrosimone
  * License: GPLv2 or later
